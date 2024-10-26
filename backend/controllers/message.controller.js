20023a2fd883bcd1167588b95233b79a69252c8c
Alex <alextanzhao22@gmail.com>
2024-10-26 15:21:06 -0700

    finish the message controller sendmessage

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,7 +1,38 @@
-export const sendMessage=(req,res)=>{
+import Conversation from '../models/conversation.model.js'
+import Message from '../models/message.model.js'
+export const sendMessage=async(req,res)=>{
+    //in this function is doing that get the recerid through the param 
+    //and get the senderid and message throuth the req.body
     try {
         const {message} = req.body
-        const {id} = req.params
+        const {id:receiverId} = req.params
+        const senderId = req.user._id
+
+        let conversation = await Conversation.findOne({
+            participants:{$all:[senderId,receiverId]}
+        })
+
+        //it because this is the first time they talk
+        if(!conversation){
+            conversation = await Conversation.create({
+                participants:[senderId,receiverId],
+
+            })
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            message,
+        })
+
+        if(newMessage){
+            conversation.messages.push(newMessage._id)
+            
+        }
+        await newMessage.save()
+        await conversation.save()
+        res.status(201).json({newMessage})
         
     } catch (error) {
         console.log('error in sending controller message',error.message)
