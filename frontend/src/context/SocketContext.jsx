f74415ad71fe148a0cdceab8004cd01460f8591e
Alex <alextanzhao22@gmail.com>
2024-11-11 16:48:55 -0800

    deployment url fix

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-g6vw.onrender.com/", {
+			const socket = io("https://chat-app-g6vw.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
